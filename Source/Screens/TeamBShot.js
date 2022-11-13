09b924baba3d7252abef82a9fbe23e1eaa200214
Tashfeen Janjua <tjanjua115@gmail.com>
2022-11-13 14:43:04 +0000

    fused Scores with Game details.

--- Source/Screens/TeamBShot.js
+++ Source/Screens/TeamBShot.js
@@ -6,8 +6,8 @@ import WhichTeamShot from './WhichTeamShot';
 
 
 const TeamBShot = ({navigation}) => {
-    const {TeamBTotalState,updateTotalB,createTotalB,ID,counterB,incrementCounterB,incrementEnd,createScores,updateScores,End,ScoresState,GameDetailsState} = useContext(ItemContext);
-    
+    const {updateTotalB,ID,counterB,incrementCounterB,incrementEnd,End,ScoresState,GameDetailsState} = useContext(ItemContext);
+    var tempScoreArray = []
 
     const [DifferentCounter, setDCounter] = useState(0);
     const found = ScoresState.find(obj => {
@@ -16,7 +16,9 @@ const TeamBShot = ({navigation}) => {
 
     let CurrentEntry = GameDetailsState.find((e) => (e.id===ID));
     console.log({CurrentEntry})
-
+    if (CurrentEntry.Scores != null || CurrentEntry.Scores != undefined){
+        tempScoreArray = CurrentEntry.Scores;
+    }
    
     const [TeamBShot, setTeamBShot] = useState(0);
     const [localcounter, setCounter] = useState(counterB);
@@ -37,156 +39,31 @@ const TeamBShot = ({navigation}) => {
         />
         <Button title = "input shot" onPress ={() => {
             if (localcounter === 0 ){
-                updateTotalB(ID,parseInt(TeamBShot));
+                let ScoretoStore = (parseInt(TeamBShot) + "B")
+                tempScoreArray.push(ScoretoStore)
+                console.log(tempScoreArray)
+                updateTotalB(ID,parseInt(TeamBShot),tempScoreArray);
+
                 setCounter(1);
                 console.log('yes');
-                let ScoretoStore = (parseInt(TeamBShot) + "B")
+   
                 console.log(ScoretoStore);
-                if (End === 1){
-                    createScores(ScoretoStore);
-                }
+
                 incrementEnd();
                 navigation.pop();
             } else {
- 
-                updateTotalB(ID,(parseInt(CurrentEntry.TeamBTotal)+parseInt(TeamBShot))) 
+                let ScoretoStore = (parseInt(TeamBShot) + "B")
+                tempScoreArray.push(ScoretoStore)  
+                console.log(tempScoreArray)
+                updateTotalB(ID,(parseInt(CurrentEntry.TeamBTotal) + parseInt(TeamBShot)),tempScoreArray) 
+
                 console.log('no'); 
 
                 incrementEnd();
                 navigation.pop();
             }
-            let ScoretoStore = (parseInt(TeamBShot) + "B")
-            if (End === 2){
-              updateScores(ID,found.ScoreEnd1,ScoretoStore)
-            } else if (End === 3){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,ScoretoStore)
-            } else if (End === 4){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,ScoretoStore)
-            } else if (End === 5){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,ScoretoStore)
-            } else if (End === 6){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,ScoretoStore)
-            } else if (End === 7){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,ScoretoStore)
-            } else if (End === 8){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,ScoretoStore)
-            } else if (End === 9){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,ScoretoStore)
-            } else if (End === 10){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,ScoretoStore)
-            } else if (End === 11){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,ScoretoStore)
-            } else if (End === 12){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,ScoretoStore)
-            } else if (End === 13){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,ScoretoStore)
-            } else if (End === 14){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,ScoretoStore)
-            } else if (End === 15){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,ScoretoStore)
-            } else if (End === 16){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,ScoretoStore)
-            } else if (End === 17){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,ScoretoStore)
-            } else if (End === 18){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,ScoretoStore)
-            } else if (End === 19){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,ScoretoStore)
-            } else if (End === 20){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,ScoretoStore)
-            } else if (End === 21){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,ScoretoStore)
-            } else if (End === 22){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,ScoretoStore)
-            } else if (End === 23){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,found.ScoreEnd22,ScoretoStore)
-            } else if (End === 24){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,found.ScoreEnd22,found.ScoreEnd23,ScoretoStore)
-            } else if (End === 25){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,found.ScoreEnd22,found.ScoreEnd23,found.ScoreEnd24,ScoretoStore)
-            } else if (End === 26){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,found.ScoreEnd22,found.ScoreEnd23,found.ScoreEnd24,found.ScoreEnd25,ScoretoStore)
-            } else if (End === 27){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,found.ScoreEnd22,found.ScoreEnd23,found.ScoreEnd24,found.ScoreEnd25,
-                    found.ScoreEnd26,ScoretoStore)
-            } else if (End === 28){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,found.ScoreEnd22,found.ScoreEnd23,found.ScoreEnd24,found.ScoreEnd25,
-                    found.ScoreEnd26,found.ScoreEnd27,ScoretoStore)
-            } else if (End === 29){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,found.ScoreEnd22,found.ScoreEnd23,found.ScoreEnd24,found.ScoreEnd25,
-                    found.ScoreEnd26,found.ScoreEnd27,found.ScoreEnd28,ScoretoStore)
-            } else if (End === 30){
-                updateScores(ID,found.ScoreEnd1,found.ScoreEnd2,found.ScoreEnd3,found.ScoreEnd4,found.ScoreEnd5,
-                    found.ScoreEnd6,found.ScoreEnd7,found.ScoreEnd8,found.ScoreEnd9,found.ScoreEnd10,
-                    found.ScoreEnd11,found.ScoreEnd12,found.ScoreEnd13,found.ScoreEnd14,
-                    found.ScoreEnd15,found.ScoreEnd16,found.ScoreEnd17,found.ScoreEnd18,found.ScoreEnd19,
-                    found.ScoreEnd20,found.ScoreEnd21,found.ScoreEnd22,found.ScoreEnd23,found.ScoreEnd24,found.ScoreEnd25,
-                    found.ScoreEnd26,found.ScoreEnd27,found.ScoreEnd28,found.ScoreEnd29,ScoretoStore)
-            }
+ 
+            
 
             }}
         />
@@ -196,19 +73,11 @@ const TeamBShot = ({navigation}) => {
         keyExtractor={(e) => e.id.toString()}
             renderItem={({item}) => {
                 return (   
-                    <><Text>{item.id}</Text><Text>{item.TeamBTotal}</Text></>
+                    <><Text>{item.id}</Text><Text>{item.TeamBTotal}</Text><Text>{item.Scores}</Text></>
                 )
             }}
         />
-         <FlatList
-        data={ScoresState}
-        keyExtractor={(e) => e.id.toString()}
-            renderItem={({item}) => {
-                return (   
-                    <><Text>{item.id}</Text><Text>{item.ScoreEnd1}</Text></>
-                )
-            }}
-        />
+        
     </View>
   );
 }

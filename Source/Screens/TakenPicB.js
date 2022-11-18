33f3aefc4dfd3ad25f655ee0563928ed0f9536e8
Tashfeen Janjua <tjanjua115@gmail.com>
2022-11-18 12:23:26 +0000

    small changes

--- Source/Screens/TakenPicB.js
+++ Source/Screens/TakenPicB.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     imageStyle:{
         maxWidth:1000,
         maxHeight:1000,
-        height: 550, 
+        aspectRatio:1,
         resizeMode: 'contain'
     }
 })
